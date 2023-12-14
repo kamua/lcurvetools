d995422afd6067f34647195bc1a242f660067bd5
Andriy <kamua@users.noreply.github.com>
2023-12-15 00:15:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup
+from setuptools import find_packages
 
 
 # __version__ = None
@@ -21,63 +22,34 @@ setup(
     package_dir = {'':'lcurvetools'},
     author = 'Andriy Konovalov',
     author_email = 'kandriy74@gmail.com',
+    license='BSD 3-Clause License',
     long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
     long_description_content_type = "text/markdown",
-    
-    '''
-    The url to where your package is stored for public view. Normally, it will be the github url to the repository you just forked.
-    '''
-    url='https://github.com/jinhangjiang/morethansentiments',
-    
-    '''
-    Leave it as deafult.
-    '''
+    url='https://github.com/kamua/lcurvetools',
     include_package_data=True,
-    
-    '''
-    This is not a enssential part. It will not affect your package uploading process. 
-    But it may affect the discoverability of your package on pypi.org
-    Also, it serves as a meta description written by authors for users.
-    Here is a full list of what you can put here:
-    
-    https://pypi.org/classifiers/
-    
-    '''
     classifiers  = [
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Development Status :: 5 - Production/Stable',
         "License :: OSI Approved :: BSD License",
-        'Intended Audience :: Developers',
-        'Intended Audience :: Other Audience',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Education',
-        'Topic :: Text Processing',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: OS Independent',
+        "Topic :: Scientific/Engineering",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+
     ],
-    
-    
-    '''
-    This part specifies all the dependencies of your package. 
-    "~=" means the users will need a minimum version number of the dependecies to run the package.
-    If you specify all the dependencies here, you do not need to write a requirements.txt separately like many others do.
-    '''
     install_requires = [
-
-        'pandas ~= 1.2.4',
-        ...
-
+        'numpy',
+        'matplotlib'
     ],
-    
-    
-    
-    '''
-    The keywords of your package. It will help users to find your package on pypi.org
-    '''
-    keywords = ['Text Mining', 'Data Science', ...],
-    
+    keywords = ['learning curve', 'keras history', 'loss_curve', 'validation_score']
 )
