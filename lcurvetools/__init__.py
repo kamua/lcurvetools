2794039a48b3d273ab7172cfdd0109c2cd2b9d63
kamua <30136613+kamua@users.noreply.github.com>
2026-02-26 11:47:03 +0200

    modified:   lcurvetools/__init__.py

--- lcurvetools/__init__.py
+++ lcurvetools/__init__.py
@@ -1,4 +1,4 @@
-from .lcurves import lcurves, lcurves_by_MLP_estimator
+from .lcurves import lcurves, lcurves_by_MLP_estimator, lcurves_by_history
 from .utils import history_concatenate
 
 # Unique source of truth for the version number.
